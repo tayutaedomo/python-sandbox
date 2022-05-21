8847e72921d86fcea0637f72805f0fc50a8e2123
tayutaedomo <tayutaedomo@gmail.com>
2022-05-21 21:00:41 +0900

    Add scripts/fire/command_chain.py

---
+++ scripts/fire/command_chain.py
@@ -0,0 +1,9 @@
+import calendar
+
+from fire import Fire
+
+if __name__ == "__main__":
+    # Ref: https://qiita.com/SaitoTsutomu/items/a5eb827737c9d59af2af
+    # python sample.py TextCalendar prmonth 2021 2
+    # = calendar.TextCalendar().prmonth(2021, 2)
+    Fire(calendar)
